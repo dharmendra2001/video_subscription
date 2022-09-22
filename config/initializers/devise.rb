70669277bac2ce2462e5f5fe76845a0a45da5039
dharmendra2001 <dharmendra@webkorps.com>
2022-09-23 02:26:07 +0530

    add google sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, 'APP_ID', 'APP_SECRET'
-
+  config.omniauth :google_oauth2, '816537586829-7rcat9m756o45c0o1ke79e5r5iahclb1.apps.googleusercontent.com', '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'
+  OmniAuth.config.allowed_request_methods= %i[get]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
