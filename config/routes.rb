0e22f69335d9391e90ad2a677d26a0b03764878f
dharmendra2001 <dharmendra@webkorps.com>
2022-09-24 03:24:09 +0530

    add show in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # get 'static_public/landing_page'
   get 'privacy', to: 'static_public#privacy'
   get 'term', to: 'static_public#term'
+
+  resources :users, only: %i[index show]
 end
 
