17c80fa6af81c5451070411556dc80f3b0ef71f5
dharmendra2001 <dharmendra@webkorps.com>
2022-09-26 21:06:12 +0530

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable,
-         :confirmable,
+        #  :confirmable,
          :omniauthable , omniauth_providers: [:google_oauth2, :github]
 
   
