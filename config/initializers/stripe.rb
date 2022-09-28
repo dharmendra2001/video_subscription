b305b5cee83bc58bf9a07eaa552b9338f81e9e25
dharmendra2001 <dharmendra@webkorps.com>
2022-09-28 19:01:41 +0530

    add stripe, add stripe customer id to user

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
 Stripe.api_key = 'sk_test_51LmkRbSETl6zkbL5NPtQiZVbGKGkn4hrtfLKUVDPsrTRXwlWx1LfECe5MuqJY0lWg24ScYNNPAcskKW7xCa3bt5T00omzjRzs5'
+Stripe.api_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :secret)
+
+# development:
+#   stripe:
+#     secret:
